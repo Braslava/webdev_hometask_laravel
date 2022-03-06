38a6086d94d2ffb16df590ba749db4b589a1a35e
Your Name <m.braslava@gmail.com>
2022-03-06 22:48:03 +0200

    adding views and linking styles

---
+++ resources/views/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Digital Marketing</title>
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+
+<body>
+    <header class="header">
+        <nav class="navbar">
+            <div class="logo">LOGO</div>
+            <ul>
+                <li>
+                    Home
+                </li>
+                <li>
+                    Services
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Contact
+                </li>
+                <li>
+                    FAQ
+                </li>
+                <li>
+                    Sing up
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="content">
+        @yield('content')
+    </div>
+    <footer class="footer"></footer>
+</body>
+
+</html>
