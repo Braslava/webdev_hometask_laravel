70c4baa3bf263ab21f481f56ff346a83b6e1f6ee
Your Name <m.braslava@gmail.com>
2022-03-06 23:59:44 +0200

    adding views and styles

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,35 +10,37 @@
 </head>
 
 <body>
-    <header class="header">
-        <nav class="navbar">
-            <div class="logo">LOGO</div>
-            <ul>
-                <li>
-                    Home
-                </li>
-                <li>
-                    Services
-                </li>
-                <li>
-                    About
-                </li>
-                <li>
-                    Contact
-                </li>
-                <li>
-                    FAQ
-                </li>
-                <li>
-                    Sing up
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <div class="content">
-        @yield('content')
+    <div class="main-container">
+        <header class="header">
+            <nav class="navbar">
+                <div class="logo">LOGO</div>
+                <ul>
+                    <li>
+                        Home
+                    </li>
+                    <li>
+                        Services
+                    </li>
+                    <li>
+                        About
+                    </li>
+                    <li>
+                        Contact
+                    </li>
+                    <li>
+                        FAQ
+                    </li>
+                    <li>
+                        <a href="" class="button">Sing up</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <section class="main">
+            @yield('content')
+        </section>
+        <footer class="footer"></footer>
     </div>
-    <footer class="footer"></footer>
 </body>
 
 </html>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,16 @@
 @extends('app')
 
 @section('content')
-Index
+<div class="home-view">
+    <div class="home-view__info">
+        <h1 class="heading1">Digital <span class="heading1-emphasis">Marketing</span></h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa, maiores doloribus. Quibusdam hic delectus perspiciatis! Iure, excepturi fuga corrupti repellendus sint obcaecati suscipit.</p>
+        <div class="buttton-wrapper">
+            <a href="#" class="button">Learn more</a>
+        </div>
+    </div>
+    <div class="home-view__img">
+        <img src="/images/illustration.jpg" alt="">
+    </div>
+</div>
 @endsection
