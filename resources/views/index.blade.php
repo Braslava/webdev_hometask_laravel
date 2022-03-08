20a40ba1cdf0d21d7a6bfeff36d3c4eb975b4069
Your Name <m.braslava@gmail.com>
2022-03-08 23:46:16 +0200

    minor improvements in styles and html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,8 +3,8 @@
 @section('content')
 <div class="home-view">
     <div class="home-view__info">
-        <h1 class="heading1">Digital <span class="bright-text">Marketing</span></h1>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam provident delectus saepe ipsa ab autem velit, error, odit corrupti, officiis veniam ullam eius.</p>
+        <h1 class="heading--main">Digital <span class="bright-text">Marketing</span></h1>
+        <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam provident delectus saepe ipsa ab autem velit, error, odit corrupti, officiis veniam ullam eius.</p>
         <div class="buttton-wrapper">
             <a href="#" class="button">Learn more</a>
         </div>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
 <div class="form-container">
+    <div class="flex-container">
+        <h1 class="heading--section">Sign up for our weekly newsletter</h1>
+        <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam provident delectus saepe ipsa ab autem velit, error, odit corrupti, officiis veniam ullam eius.</p>
+    </div>
     <form action="" class="registration-form">
         <label for="name">Name <input type="text" placeholder="name"></label>
         <label for="e-mail">E-mail <input type="text" placeholder="e-mail"></label>
