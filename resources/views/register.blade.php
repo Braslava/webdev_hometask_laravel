3bbc8cc3972e2e317018ba3a68547e2a8c1c2664
Your Name <m.braslava@gmail.com>
2022-03-10 22:31:18 +0200

    connecting to mysql database and creating a migration for registrations

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -6,10 +6,16 @@
         <h1 class="heading--section">Sign up for our weekly newsletter</h1>
         <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam provident delectus saepe ipsa ab autem velit, error, odit corrupti, officiis veniam ullam eius.</p>
     </div>
-    <form action="" class="registration-form">
-        <label for="name">Name <input type="text" placeholder="name"></label>
-        <label for="e-mail">E-mail <input type="text" placeholder="e-mail"></label>
-        <button class="button--outline">Register</button>
+    <form action="" method="POST" class="registration-form">
+        <label for="name">Name <input type="text" id="name" name="name" placeholder="name"></label>
+        <label for="email">E-mail <input type="text" id="email" name="email" placeholder="email"></label>
+        <button type="submit" class="button--outline">Register</button>
     </form>
 </div>
 @endsection
+
+<!-- 1. Sadaļā "Sign up" pievienot formu.
+	2. Forma satur 2 laukus (name un e-mail) un submit pogu.
+	3. Izmantojot "migration scripts" izveidot jaunu DB tabulu.
+	4. Pēc formas aizpildīšanas un nosūtīšanas, jāparādās paziņojumam.
+	5. Datiem jāsaglabājas DB tabulā. -->
