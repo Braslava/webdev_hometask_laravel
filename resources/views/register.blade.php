c96067619e1d17307a9ec1b7be0917310007b9f6
Your Name <m.braslava@gmail.com>
2022-03-13 09:16:32 +0200

    adding validation

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -7,7 +7,7 @@
         <p class="paragraph">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magnam provident delectus saepe ipsa ab autem velit, error, odit corrupti, officiis veniam ullam eius.</p>
         <p class="success-msg">{{ session('message') }}</p>
     </div>
-    @if ($errors->any())
+    <!-- @if ($errors->any())
     <div class="error-msg">
         <ul>
             @foreach ($errors->all() as $error)
@@ -15,12 +15,14 @@
             @endforeach
         </ul>
     </div>
-    @endif
+    @endif -->
     <form action="" method="POST" class="registration-form">
         @csrf
 
         <label for="name">Name <input type="text" id="name" name="name" placeholder="name"></label>
+        <p class="error-msg">@error('name'){{ $message }}@enderror</p>
         <label for="email">E-mail <input type="text" id="email" name="email" placeholder="email"></label>
+        <p class="error-msg">@error('email'){{ $message }}@enderror</p>
         <button type="submit" class="button--outline">Register</button>
     </form>
 </div>
